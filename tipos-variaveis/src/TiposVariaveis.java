0a477c5734ed9f183cf7bb1034fec487e4d43f92
Carlos Eduardo <carloseduardo15000@gmail.com>
2024-03-01 20:28:35 -0300

    novos testes com variaveis 20:28

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        final double VALOR_DE_PI = 3.14;
+        System.out.println(VALOR_DE_PI);
     }
 }
