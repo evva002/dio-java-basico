3594596e3a14954d3c3a3356c392eb0d213bf1a7
Carlos Eduardo <carloseduardo15000@gmail.com>
2024-03-06 07:53:28 -0300

    operador térnario

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        int resultado = a==b ? 1 : 0;
+
+        System.out.println(resultado);
+    }
+}
