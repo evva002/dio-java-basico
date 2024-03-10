8abdf5baa31f0de458387da6d2e1ce96afc8beec
Carlos Eduardo <carloseduardo15000@gmail.com>
2024-03-10 18:13:47 -0300

    Testando a classe Scanner

---
+++ teste-java-scanner/src/TesteScanner.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TesteScanner {
+    public static void main(String[] args)  {
+        //lendo os dados
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite a sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite a sua altura: ");
+        double altura = scanner.nextDouble();
+
+        //imprimindo os dados
+
+        System.out.println("Olá, meu nome é: " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é: " + altura + "cm");
+
+    }
+}
